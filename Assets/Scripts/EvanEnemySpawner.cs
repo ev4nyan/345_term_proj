5f598dec7b09bd7d8f4f96c7cc108fa63c2a7f99
ev4nyan <49965629+ev4nyan@users.noreply.github.com>
2025-12-07 12:47:59 -0500

    bla

--- Assets/Scripts/EvanEnemySpawner.cs
+++ Assets/Scripts/EvanEnemySpawner.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour
+public class EvanEnemySpawner : MonoBehaviour
 {
     [Header("Enemy Prefabs")]
     public GameObject slimePrefab;
